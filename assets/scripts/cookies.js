30a01ba2fec741c4d72e3b2c3777972d1dceece5
Greg <x@x.org>
2023-03-01 09:55:50 +0100

    cookies

--- assets/scripts/cookies.js
+++ assets/scripts/cookies.js
@@ -8,23 +8,30 @@ function displayCookiesMessage() {
 
 function acceptCookies() {
     console.log("Cookies accepted");
+    hideCookiesMessage();
 }
 
 function rejectCookies() {
     console.log("Cookies rejected");
+    hideCookiesMessage();
 }
 
 
 async function sleep(duration) {
-
     return new Promise(resolve => setInterval(resolve, duration))
 }
 
 async function cookiesProcess()
 {
+    const rejectButton = document.getElementById("reject_cookies")
+    const acceptButton = document.getElementById("accept_cookies")
+
     await sleep(2000)
     displayCookiesMessage()
 
+    rejectButton.addEventListener("click", rejectCookies)
+    acceptButton.addEventListener("click", acceptCookies)
+    
 }
 
 await cookiesProcess();
